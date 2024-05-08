0fa707435e2d524690f2714701b8d7c995b06b8c
melikeekara3 <melikekara333@gmail.com>
2024-05-08 21:48:34 +0300

    sucideyiz

--- powwww/src/main/java/org/example/powwww/SceneController.java
+++ powwww/src/main/java/org/example/powwww/SceneController.java
@@ -1,27 +1,32 @@
 package org.example.powwww;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 
 public class SceneController {
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button signUpButton;
+    @FXML
+    private TextField userNameTextField;
+    @FXML
+    private TextField passwordTextField;
+
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    public void switchToLogInPage(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("/org/example/powwww/Merhaba.fxml"));
-        stage = (Stage)(((Node)event.getSource()).getScene().getWindow());
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
     public void switchToSignUpPage(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("/org/example/powwww/SignInPage.fxml"));
         stage = (Stage)(((Node)event.getSource()).getScene().getWindow());
