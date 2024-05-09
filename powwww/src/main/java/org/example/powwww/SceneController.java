1d7b73b9dec22901840600f55767ddd3a86cc487
melikeekara3 <melikekara333@gmail.com>
2024-05-10 00:02:26 +0300

    sign in and log in
    it gives error message when you give wrong info

--- powwww/src/main/java/org/example/powwww/SceneController.java
+++ powwww/src/main/java/org/example/powwww/SceneController.java
@@ -1,5 +1,7 @@
 package org.example.powwww;
 
+import javafx.scene.control.Alert;
+import org.example.powwww.Sim.UserMethods;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.example.powwww.Sim.UserMethods;
 
 import java.io.IOException;
 
@@ -18,6 +21,10 @@ public class SceneController {
     private Button loginButton;
     @FXML
     private Button signUpButton;
+    @FXML
+    private Button signUpButtonTwo;
+    @FXML
+    private Button signUpButtonThree;
     @FXML
     private TextField userNameTextField;
     @FXML
@@ -29,7 +36,52 @@ public class SceneController {
     private FXMLLoader fxmlLoader;
 
     public void switchToSignUpPage(ActionEvent event) throws IOException {
+        fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/powwww/SignInForNursesAndCourier.fxml"));
+        root = fxmlLoader.load();
+        scene = new Scene(root);
+        scene.setRoot(root);
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToSignUpPage2(ActionEvent event) throws IOException {
         fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/powwww/SignInPage.fxml"));
+        root = fxmlLoader.load();
+        scene = new Scene(root);
+        scene.setRoot(root);
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToSignUpPagetoLogIn(ActionEvent event) throws IOException {
+        fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/powwww/Merhaba.fxml"));
+        root = fxmlLoader.load();
+        scene = new Scene(root);
+        scene.setRoot(root);
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToHomePageWithLogIn(ActionEvent event) throws IOException {
+        if(UserMethods.login(userNameTextField.getText(), passwordTextField.getText())) {
+            fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/powwww/HomePage.fxml"));
+            root = fxmlLoader.load();
+            scene = new Scene(root);
+            scene.setRoot(root);
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        }
+        else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Check your personal information, and try to Login");
+            alert.setContentText("You might type wrong user name or password");
+            alert.setHeaderText("Information Error");
+            alert.showAndWait();
+        }
+    }
+    public void switchToHomePageInApp(ActionEvent event) throws IOException {
+        fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/powwww/HomePage.fxml"));
         root = fxmlLoader.load();
         scene = new Scene(root);
         scene.setRoot(root);
